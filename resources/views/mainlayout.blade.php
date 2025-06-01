101649e516f6dd41f3a8188c45b8613cc72f1a49
ZonkDBR <crazyzonkkopong@gmail.com>
2025-06-01 19:47:32 +0700

    CSRF-Fix-2

--- resources/views/mainlayout.blade.php
+++ resources/views/mainlayout.blade.php
@@ -4,9 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>@yield('title', 'Default Title')</title>
-    <!-- Include other necessary meta tags and stylesheets -->
-    @include('layout.styles')
 </head>
 <body>
     @include('layout.header')
@@ -16,7 +13,5 @@
     </main>
 
     @include('layout.footer')
-    <!-- Include JavaScript files -->
-    @include('layout.scripts')
 </body>
 </html>
